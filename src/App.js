2700b37838009dc8f4fe8634358f759ea3c3d4b2
username <15305383+Int314@users.noreply.github.com>
2023-02-12 17:17:08 +0900

    デザイン修正、削除ボタン追加

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import { useState, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: uuidv4(), name: "ご飯をたべる", completed: false },
+    { id: uuidv4(), name: "お風呂に入る", completed: false },
+    { id: uuidv4(), name: "歯を磨く", completed: false },
+    { id: uuidv4(), name: "Hello World !!!", completed: true },
+  ]);
 
   const todoNameRef = useRef();
 
@@ -12,6 +17,7 @@ function App() {
     // タスクを追加
     const name = todoNameRef.current.value;
 
+    // ブランクの場合処理しない
     if (name === "") return;
 
     setTodos((prevTodos) => {
@@ -25,7 +31,10 @@ function App() {
     const newTodos = [...todos];
     const todo = newTodos.find((todo) => todo.id === id);
     todo.completed = !todo.completed;
+
     setTodos(newTodos);
+
+    sortTodo();
   };
 
   const handleClear = () => {
@@ -34,13 +43,67 @@ function App() {
     setTodos(newTodos);
   };
 
+  const deleteTodo = (id) => {
+    // Todo削除
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const sortTodo = () => {
+    const newTodos = [...todos];
+    newTodos.sort((a, b) => {
+      if (a.completed) {
+        return 1;
+      }
+      if (b.completed) {
+        return -1;
+      }
+
+      return 0;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input type="text" ref={todoNameRef}></input>
-      <button onClick={handleAddTodo}>タスクを追加</button>
-      <button onClick={handleClear}>完了したタスクの削除</button>
-      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
+      <div className="container px-4 max-w-lg">
+        <div className="flex items-center gap-4">
+          <input
+            type="text"
+            className="input input-bordered input-md w-2/3"
+            ref={todoNameRef}
+            autoFocus
+          ></input>
+          <button
+            className="btn btn-outline btn-success btn-sm"
+            onClick={handleAddTodo}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="bi bi-check h-6 w-6"
+              viewBox="0 0 16 16"
+            >
+              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+            </svg>
+            追加
+          </button>
+        </div>
+        <div>
+          残りのタスク：{todos.filter((todo) => !todo.completed).length}
+          <button
+            className="btn btn-link text-info no-underline"
+            onClick={handleClear}
+          >
+            完了したタスクの削除
+          </button>
+        </div>
+        <TodoList
+          todos={todos}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      </div>
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import Todo from './Todo'
+import React from "react";
+import Todo from "./Todo";
 
-const TodoList = ({ todos, toggleTodo }) => {
-  return todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />);
-}
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+  return todos.map((todo) => (
+    <Todo
+      todo={todo}
+      key={todo.id}
+      toggleTodo={toggleTodo}
+      deleteTodo={deleteTodo}
+    />
+  ));
+};
 
-export default TodoList
+export default TodoList;
